747dabf7626bceae966a1315e1d6467c5ef9d3e4
Brett McGinnis <brett.mcginnis@apptentive.com>
2019-11-25 14:16:42 -0800

    Added filtering by name (#16)

--- src/restaurant/list.js
+++ src/restaurant/list.js
@@ -1,8 +1,25 @@
+const Fuse = require('fuse.js');
 const storage = require('../storage');
 
-async function list() {
-  const r = await storage.getAllRestaurants();
-  return r;
+// criteria is optional and an object of and filters
+// {
+//   names: ['sushi', 'kudasai'],
+// }
+async function list(criteria) {
+  let rs = await storage.getAllRestaurants();
+
+  if (criteria && criteria.names) {
+    criteria.names.forEach((name) => {
+      const nameOpts = {
+        keys: ['name'],
+      };
+
+      const nameFuse = new Fuse(rs, nameOpts);
+      rs = nameFuse.search(name);
+    });
+  }
+
+  return rs;
 }
 
 module.exports = {
---
+++ src/restaurant/list.test.js
@@ -0,0 +1,46 @@
+const storage = require('../storage');
+const { list } = require('./list');
+const { add } = require('./add');
+
+beforeEach(() => {
+  storage.resetDb();
+});
+
+const ludisId = 'ultF6EfzuR_S3-QJyx67rw';
+const crumpetId = 'aX2ctpgS9uvFDfdzCXjecA';
+
+test('should be able to find ludis', async () => {
+  let rs = await list();
+  expect(rs.length).toBe(0);
+
+  await add(ludisId);
+  rs = await list();
+  expect(rs.length).toBe(1);
+
+  const results = await list({
+    names: ['lud'],
+  });
+
+  console.log(results);
+  expect(results.length).toBe(1);
+});
+
+test('should be able to find ludis with crumpets around', async () => {
+  let rs = await list();
+  expect(rs.length).toBe(0);
+
+  await add(ludisId);
+  rs = await list();
+  expect(rs.length).toBe(1);
+
+  await add(crumpetId);
+  rs = await list();
+  expect(rs.length).toBe(2);
+
+  const results = await list({
+    names: ['lud'],
+  });
+
+  console.log(results);
+  expect(results.length).toBe(1);
+});
