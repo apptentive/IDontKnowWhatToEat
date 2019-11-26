e7c2880a976462d233891d7d083d09b3320e738e
Brett McGinnis <brett.mcginnis@apptentive.com>
2019-11-26 09:40:54 -0800

    added loves api

--- src/love/endpoint.js
+++ src/love/endpoint.js
@@ -1,12 +1,15 @@
 const { list } = require('../restaurant');
-// const { listUsers } = require('../user');
+const { list: listUsers } = require('../user');
 
 async function loveHandler(req, res) {
   const restaurants = await list();
-  // const users = await listUsers();
+  const users = await listUsers();
 
-  // TODO: Count restaurant id count across users
-  // const rs = restaurants.map(r => )
+  restaurants.forEach((r) => {
+    const loves = users.map((u) => u.loves.filter((l) => l === r.id));
+    // eslint-disable-next-line no-param-reassign
+    r.loveCount = loves.reduce((p, c) => p.length + c.length);
+  });
 
   res.send(restaurants);
 }
